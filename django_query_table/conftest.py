ec97c9de8adca805f855a20fafca427693dd8ca7
windalfin <windalfin@treeclouds.com>
2024-05-15 10:15:41 +0700

    first commit

---
+++ django_query_table/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_query_table.users.models import User
+from django_query_table.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
